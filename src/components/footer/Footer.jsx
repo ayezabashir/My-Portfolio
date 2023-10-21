25b8d6816d18d1f22725b8a08040a173161c7328
Ayeza <ayezabashir442@gmail.com>
2023-10-21 22:21:18 +0500

    footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,40 +1,32 @@
-import { BsGithub, BsInstagram, BsLinkedin } from 'react-icons/bs'
-import { FiCodepen, FiMail } from 'react-icons/fi'
+import { BsGithub, BsLinkedin } from 'react-icons/bs'
+import { FiCodepen } from 'react-icons/fi'
 const Footer = () => {
     return (
         <>
             <footer>
                 <div className="container">
-                    <h2>Contact Me</h2>
-                    <ul className="footer-list">
-                        <li>
-                            <a href="">
-                                <BsGithub className='social' />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <BsLinkedin className='social' />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <BsInstagram className='social' />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <FiCodepen className='social' />
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <FiMail className='social' />
-                            </a>
-                        </li>
-                    </ul>
-                    <br /><br />
-                    <small>Copyright &copy; 2023, Ayeza Bashir</small>
+                    <div className="footer-content">
+                        <h2>Contact Me</h2>
+                        <ul className="footer-list">
+                            <li className='footer-list-item'>
+                                <a href="https://github.com/ayezabashir">
+                                    <BsGithub className='icon social' />
+                                </a>
+                            </li>
+                            <li className='footer-list-item'>
+                                <a href="https://www.linkedin.com/in/ayezabashir">
+                                    <BsLinkedin className='icon social' />
+                                </a>
+                            </li>
+                            <li className='footer-list-item'>
+                                <a href="https://codepen.io/ayezabashir">
+                                    <FiCodepen className='icon social' />
+                                </a>
+                            </li>
+                        </ul>
+                        <br /><br />
+                        <small className='copyrights'>Copyright &copy; 2023, Ayeza Bashir</small>
+                    </div>
                 </div>
             </footer>
         </>
