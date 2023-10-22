7af6300d34f513e9f5a37a3b50040f85643813f1
Ayeza <ayezabashir442@gmail.com>
2023-10-22 22:27:06 +0500

    contact form

--- src/components/contactform/ContactForm.jsx
+++ src/components/contactform/ContactForm.jsx
@@ -5,21 +5,28 @@ const ContactForm = () => {
         <>
             <div className="contact-form">
                 <div className="container">
-                    <form className='form'>
-                        <div className="form-element">
-                            <label htmlFor="fullname">FullName</label>
-                            <input type="text" id='fullname' name='fullname' />
-                        </div>
-                        <div className="form-element">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" name="email" id="email" />
-                        </div>
-                        <div className="form-element">
-                            <label htmlFor="msg">Your Message</label>
-                            <textarea name="msg" id="msg" cols="30" rows="10"></textarea>
-                        </div>
-                        <button type="submit" className="form-submit">Submit</button>
-                    </form>
+                    <div className="contact-content">
+                        <h2>Contact Me</h2>
+                        <form className='form'>
+                            <div className="left">
+                                <div className="form-element">
+                                    <label htmlFor="fullname">Full Name</label>
+                                    <input required type="text" id='fullname' name='fullname' />
+                                </div>
+                                <div className="form-element">
+                                    <label htmlFor="email">Email</label>
+                                    <input required type="email" name="email" id="email" />
+                                </div>
+                            </div>
+                            <div className="right">
+                                <div className="form-element">
+                                    <label htmlFor="msg">Your Message</label>
+                                    <textarea required name="msg" id="msg" cols="30" rows="6"></textarea>
+                                </div>
+                                <button type="submit" className="form-submit">Submit</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </>
