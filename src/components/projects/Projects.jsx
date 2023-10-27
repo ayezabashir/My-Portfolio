79b9db06b2fa42ac9cba3fbcae06d6094fa6290c
Ayeza <ayezabashir442@gmail.com>
2023-10-27 23:18:34 +0500

    project component

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -10,6 +10,7 @@ const allProjects = [
         id: 1,
         project_cover: abelton,
         project_name: 'Abelton Clone',
+        project_desc: 'A resposive ReactJS and Sass-powered Ableton website clone - A frontend-practice project challenge.',
         project_languages: ['reactjs', 'scss'],
         project_live_link: 'https://ableton-clone-six.vercel.app/',
         project_github_link: 'https://github.com/ayezabashir/Ableton-Clone'
@@ -18,6 +19,7 @@ const allProjects = [
         id: 2,
         project_cover: booknest,
         project_name: 'BookNest - Search Books',
+        project_desc: 'Developed using ReactJS and styled with Sass, BookNest leverages the OpenLibrary API to seamlessly fetch and present book data.',
         project_languages: ['reactjs', 'react-router', 'scss', 'api'],
         project_live_link: 'https://book-nest-search-books.vercel.app/',
         project_github_link: 'https://github.com/ayezabashir/BookNest-SearchBooks'
@@ -26,6 +28,7 @@ const allProjects = [
         id: 3,
         project_cover: crypto,
         project_name: 'CryptoCurrency Search',
+        project_desc: 'Utilizing Axios, real-time CoinGecko data is fetched for comprehensive currency insights. A simplified way to access essential cryptocurrency information',
         project_languages: ['reactjs', 'react-bootstrap', 'axios', 'api'],
         project_live_link: 'https://book-nest-search-books.vercel.app/',
         project_github_link: 'https://github.com/ayezabashir/BookNest-SearchBooks'
@@ -55,11 +58,14 @@ const Projects = () => {
                                                 </div>
                                                 <div className="project-desc">
                                                     <div className="desc">
-                                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates odit provident at quisquam.</p>
+                                                        <p>{item.project_desc}</p>
                                                     </div>
                                                     <div className="languages">
-                                                        <span>html</span>
-                                                        <span>css</span>
+                                                        {
+                                                            item.project_languages.map((lang, index) => (
+                                                                <span className='lang' key={index}>{lang}</span>
+                                                            ))
+                                                        }
                                                     </div>
                                                     <div className="links">
                                                         <div className="website">
@@ -82,7 +88,7 @@ const Projects = () => {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
