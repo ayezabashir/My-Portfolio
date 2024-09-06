9f8c3a5c240a12cb7395dec8523c84cd2c95619e
Ayeza Bashir <ayezabashir442@gmail.com>
2024-09-06 22:18:47 +0500

    Update Main.jsx

--- src/components/mainintro/Main.jsx
+++ src/components/mainintro/Main.jsx
@@ -6,7 +6,10 @@ const Main = () => {
                     <div className="main-body">
                         <div className="main-content">
                             <div className="main-title">
-                                <h1>Asslamo Alaikum! ✌🏻 I&apos;m <span className="highlight">Ayeza Bashir</span> </h1>
+                                <h1>
+                  Asslamo Alaikum! <span className="emoji">✌🏻</span> I&apos;m{" "}
+                  <span className="highlight">Ayeza Bashir</span>{" "}
+                </h1>
                             </div>
                             <div className="main-subtitle">
                                 <h3>Aspiring Software Developer enthusiastic about creating interactive and user-friendly web experiences. Always exploring new ways to stay at the forefront of technologies.</h3>
