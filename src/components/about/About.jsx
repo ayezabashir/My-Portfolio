0be4eb63160b378d99e7be45752b5855a402dfd7
Ayeza Bashir <ayezabashir442@gmail.com>
2025-07-31 17:23:04 +0000

    about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,13 +12,16 @@ const About = () => {
                     <div className="about-content">
                         <h2>About Me</h2>
                         <div className="about-me">
-                            <p className='name'>Hi my name is Ayeza. I am 22 years old. I have Bachelors degree in <span className='highlight'>Software Engineering</span> and currently I am doing MPhill in Software Engineering.</p> <br />
+                            <p className='name'>Hi my name is Ayeza. I recently did my Masters in <span className='highlight'>Software Engineering</span></p> <br />
                             <p>I started learning <span className="highlight">Web Development</span> after my degree in 2022. It was easy for me to grasp that as I already had a programming background.
                                 I started from the very start HTML and CSS and worked my way up to JavaScript and practicing building simple projects. I moved to advanced JS courses and started working on building my logic skills.
-                                I did Leetcode and Codewars challenges now and then and it really helped me open my mind. I started uploading my projects on github and it really boosted my confidence after watching the green dots displaying how consistent
-                                I have been in coding and I have not stopped since then.
-                            </p> <br />
-                            <p>I still have lots to learn but I am consistent and willing to take challenges.</p>
+                                I did Leetcode and Codewars challenges now and then and it really helped me open my mind. I started uploading my projects on github and it really boosted my confidence.
+                            </p> 
+                            <p>My interest in <span className="highlight">Artificial Intelligence</span> rose while I was working on my research. My
+scholarly experience is based on <span className="highlight">deep learning</span> and computer vision, as it led me to conduct a research
+project that aims to achieve image understanding using Vision Transformer (ViT) models. I would like to
+be part of the most innovative AI technologies and use my technology in computer vision and machine
+learning to solve real-life problems.</p>
                         </div>
                         <div className="current-stack">
                             <h3>My CurrentStack of <span className="highlight">languages/technologies</span></h3>
